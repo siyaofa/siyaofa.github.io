8dbc1ad93b4a237f376aeb10915f154665a493cc
siyaofa <siyaofa@outlook.com>
2019-05-29 02:01:03 +0800

    Create default.js

---
+++ config/default.js
@@ -0,0 +1,11 @@
+MathJax.Hub.Config({
+    showProcessingMessages: false,
+    jax: ["input/TeX", "output/HTML-CSS"],
+    TeX: {
+      TagSide: "left",
+      Macros: {
+        RR: '{\\bf R}',
+        bold: ['{\\bf #1}',1]
+      }
+    }
+  });
