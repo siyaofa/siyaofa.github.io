2835d73830909e6d0d340be2e161f7115dbdb0ca
siyaofa <siyaofa@outlook.com>
2019-06-14 01:18:18 +0800

    Update notch_filter.m

--- pages/controller/src/notch_filter.m
+++ pages/controller/src/notch_filter.m
@@ -1,3 +1,6 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%SMEE 陷波器
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear;close all;clc;
 zero_freq=50;
 zero_omega=2*pi*zero_freq;
@@ -14,7 +17,29 @@ notch=tf([zero_omega^2 2*zero_omega*zero_damp 1],
 figure
 bode(notch)
 
-omega_n=zero_omega;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%NYU 陷波器
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear;close all;clc;
+pkg load control
+%抑制频率为50Hz
+f_n=50;
+omega_n=2*pi*f_n;
 notch=tf([1 0 omega_n^2],[1 2*omega_n omega_n^2]);
 figure
 bode(notch)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%带阻尼的陷波器
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear;close all;clc;
+pkg load control
+%抑制频率为50Hz
+f_n=50;
+%阻尼系数
+damping=3;
+omega_n=2*pi*f_n;
+notch=tf([1 2*omega_n omega_n^2],[1 2*damping*omega_n omega_n^2]);
+figure
+bode(notch)
+
