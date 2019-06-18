7b82c6e72c03ec4a11624cd2dc56934f20db0140
siyaofa <siyaofa@outlook.com>
2019-06-19 02:21:36 +0800

    update

--- pages/project/src/octave_IMU/arduino_gy85_demo.m
+++ pages/project/src/octave_IMU/arduino_gy85_demo.m
@@ -1,15 +1,19 @@
 close all;clear;clc;
 
-sample_num=1000;
+sample_num=5000;
 
 #静止状态下采磁力计和陀螺仪数据
-[~,gyro_data,compass_data] = gy85_sample(sample_num);
-
-save gyro_data gyro_data;
-save compass_data compass_data;
+[acc_data,gyro_data,compass_data] = gy85_sample(sample_num);
 
 gyro_offset= mean(gyro_data,1);
 compass_offset= mean(compass_data,1);
+
+save gyro_data gyro_data gyro_offset;
+save compass_data compass_data compass_offset;
+
+figure
+scatter3(acc_data(:,1),acc_data(:,2),acc_data(:,3))
+title('acc')
 
 figure
 scatter3(gyro_data(:,1),gyro_data(:,2),gyro_data(:,3),'.')
@@ -40,6 +44,9 @@ f=@(p,data)((data(:,1)-p(1))/p(2)).^2 ...
 
 ## 拟合求得加速度偏心 各轴比例系数
 p=nlinfit(acc_data,zeros(size(acc_data,1),1),f,[0 1 0 1 0 1]);
+#保存数据
+acc_offset=p;
+save acc_data acc_offset acc_data;
 
 acc_calibration=[(acc_data(:,1)-p(1))/p(2),(acc_data(:,2)-p(3))/p(4),((acc_data(:,3)-p(5))/p(6))];
 
