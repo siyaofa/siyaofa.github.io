fea7120b90f0b8da14d5f05f7c84b2474208a3e5
siyaofa <siyaofa@outlook.com>
2019-12-05 01:20:59 +0800

    可用

--- pages/algorithm/src/tensorflow_test/cube_classify/vott_csv_test/csv_to_tfrecord.py
+++ pages/algorithm/src/tensorflow_test/cube_classify/vott_csv_test/csv_to_tfrecord.py
@@ -1,28 +1,46 @@
 import pandas as pd
 import tensorflow as tf
+import os
+import cv2 as cv
 
-def create_tf_example(image,features, label):
 
+def create_tf_example(filename,encoded,features, label):
+    img=cv.imread(filename)
+    # print(img.shape)
+    height=img.shape[0]
+    width=img.shape[1]
+    xmins = [features[0] / width]
+    
+    ymins = [features[1] / height]
+    xmaxs = [features[2] / width]
+    ymaxs = [features[2]/ height]
     tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image':tf.train.Feature(bytes_list=tf.train.BytesList(value=[image.encode('utf-8')])),
-        'xmin':tf.train.Feature(float_list=tf.train.FloatList(value=[features[0]])),
-        'ymin':tf.train.Feature(float_list=tf.train.FloatList(value=[features[1]])),
-        'xmax':tf.train.Feature(float_list=tf.train.FloatList(value=[features[2]])),
-        'ymax':tf.train.Feature(float_list=tf.train.FloatList(value=[features[3]])),
-        'label':tf.train.Feature(bytes_list=tf.train.BytesList(value=[label.encode('utf-8')])),
+        'image/width':tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
+        'image/height':tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
+        'image/filename':tf.train.Feature(bytes_list=tf.train.BytesList(value=[filename.encode('utf-8')])),
+        'image/encoded':tf.train.Feature(bytes_list=tf.train.BytesList(value=[filename.encode('utf-8')])),
+        'image/object/bbox/xmin':tf.train.Feature(float_list=tf.train.FloatList(value=xmins)),
+        'image/object/bbox/ymin':tf.train.Feature(float_list=tf.train.FloatList(value=ymins)),
+        'image/object/bbox/xmax':tf.train.Feature(float_list=tf.train.FloatList(value=xmaxs)),
+        'image/object/bbox/ymax':tf.train.Feature(float_list=tf.train.FloatList(value=ymaxs)),
+        'image/object/class/text':tf.train.Feature(bytes_list=tf.train.BytesList(value=[label.encode('utf-8')])),
+        'image/object/class/label':tf.train.Feature(int64_list=tf.train.Int64List(value=[0])),
     }))
     return tf_example
 
 
-
-csv = pd.read_csv(r"D:\TEMP\cube_classify\raw_from_user\vott\vott-csv-export\cube4points-export.csv").values
+image_path=r'D:\TEMP\cube_classify\raw_from_user\vott\vott-csv-export'
+csv_filename=r'cube4points-export.csv'
+csv = pd.read_csv(os.path.join(image_path,csv_filename)).values
 
 with tf.io.TFRecordWriter("csv.tfrecords") as writer:
     line_num=0
     for row in csv:
         line_num+=1
-        if(line_num>1) :
+        if(line_num>1) and (line_num<=10) :
             image, features, label = row[0],row[1:-1], row[-1]
-            print(image, features, label)
-            example = create_tf_example(image,features, label)
+            #print(image, features, label)
+            example = create_tf_example(filename=os.path.join(image_path,image),encoded='PNG',features=features, label=label)
             writer.write(example.SerializeToString())
+        elif line_num>10:
+            break
